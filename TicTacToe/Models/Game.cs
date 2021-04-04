6feaf5c0323c72f6f1f5c3b294a9f2b43fde6004
MaximusXI <harshilmakwana34@gmail.com>
2021-04-04 14:27:56 +0530

    HM's First commit

---
+++ TicTacToe/Models/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Models
+{
+    public class Game
+    {
+        public void clicked(string id)
+        {
+            Console.WriteLine("clicked");
+     
+        }
+    }
+}
